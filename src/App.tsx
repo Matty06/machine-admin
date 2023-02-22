765ac622a5281eabbc932c02243f4cc7a3552d4c
Matěj Kunc <matej.kunc@centrum.cz>
2023-02-22 10:44:26 +0100

    Added: card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Card from './Components/Card'
 import './styles/styles.scss';
 
 function App() {
@@ -7,6 +8,13 @@ function App() {
 return (
     <div className="App">
       <Header />
+      <div className="cards-wrapper">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Footer />
     </div>
   )
---
+++ src/Components/Card.tsx
@@ -0,0 +1,12 @@
+import '../styles/card.scss'
+
+function Card() {
+
+return (
+        <div className="card">
+            <h1 className="card__title">Machine 1</h1>
+        </div>
+	)
+}
+
+export default Card
